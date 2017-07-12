1cd979367be1403a49c3cebc53b88d834366d992
kingflag <1362168862@qq.com>
2017-07-12 14:29:51 +0800

    整合hibernate失败，

--- maven-core/src/main/java/com/king/demo/maven/dao/ReadJobDaoImpl.java
+++ maven-core/src/main/java/com/king/demo/maven/dao/ReadJobDaoImpl.java
@@ -18,6 +18,7 @@ public class ReadJobDaoImpl implements IReadJobDao {
   public void setDataSource(DataSource dataSource) {
     this.jdbcTemplate = new JdbcTemplate(dataSource);
   }
+  
 
   @Override
   public List<HdUserinfo> selectall() throws Exception {
--- maven-core/src/main/java/com/king/demo/maven/domain/HdUserinfo.java
+++ maven-core/src/main/java/com/king/demo/maven/domain/HdUserinfo.java
@@ -1,14 +1,36 @@
 package com.king.demo.maven.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class HdUserinfo {
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+
+//@Entity
+//@Table(name = HdUserinfo.TABLE_NAME)
+public class HdUserinfo implements Serializable  {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  /** 数据库表名称。 */
+  public static final String TABLE_NAME = "hdUserinfo";
 
   private int id;
   private String user;
   private String surplus;
   private Date createtime;
 
+//  @Id
+//  @Column(name = "id")
+//  @GeneratedValue(strategy=GenerationType.AUTO)
   public int getId() {
     return id;
   }
@@ -17,6 +39,7 @@ public class HdUserinfo {
     this.id = id;
   }
 
+  //@Column(name = "user")
   public String getUser() {
     return user;
   }
@@ -25,6 +48,7 @@ public class HdUserinfo {
     this.user = user;
   }
 
+  //@Column(name = "surplus")
   public String getSurplus() {
     return surplus;
   }
@@ -33,6 +57,7 @@ public class HdUserinfo {
     this.surplus = surplus;
   }
 
+  //@Column(name = "surplus")
   public Date getCreatetime() {
     return createtime;
   }
